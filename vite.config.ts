07b9849f32b52ea82379b3508138df0536a3aed3
Ethan Yang <ttethanyang@gmail.com>
2025-11-15 00:51:44 -0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  optimizeDeps: {
+    include: [
+      "@radix-ui/react-dropdown-menu",
+      "@radix-ui/react-slot",
+      "next-themes",
+    ],
+  },
+});
